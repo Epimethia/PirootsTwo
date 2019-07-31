7e3d43e02953b02b7c5210ab0cbf310433badbce
James Devenport <jaden.links@yahoo.com>
2019-08-01 00:42:07 +1200

    Player Movement, Camera and Menu Complete

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float m_fPlayerSpeed = 6.0f;
+
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal") * m_fPlayerSpeed * Time.deltaTime;
+        float v = Input.GetAxis("Vertical") * m_fPlayerSpeed * Time.deltaTime;
+
+        Vector3 MovementDirection = new Vector3(h, 0.0f, v);
+
+        transform.position += MovementDirection;
+    }
+}
